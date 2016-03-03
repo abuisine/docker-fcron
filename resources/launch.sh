1b8c36fac52414b7571fdec45368daa01156666e
Alexandre Buisine <alexandrejabuisine@gmail.com>
2016-03-03 23:24:20 +0100

    do not show configuration as it may contain passphrases

--- resources/launch.sh
+++ resources/launch.sh
@@ -4,10 +4,10 @@ DELAY=2
 echo "-----------------------"
 echo "Setting environment variables in fcrontabs"
 /usr/local/bin/ep *.fcrontab
-echo "Injecting the following configuration in background (${DELAY}s delay) :"
-awk 'FNR==1{print ""}1' *.fcrontab
+echo "Injecting the fcrontab in background (${DELAY}s delay)."
 (
 	sleep $DELAY
+	echo "Injecting ..."
 	awk 'FNR==1{print ""}1' *.fcrontab | fcrontab -n - && echo "Injection successful" || echo "Injection failed"
 ) &
 echo
